a5be2bf0814877aa3ffc0c7c11e7d65c9fc356f1
Jatin Goyal <goyaljatin344@gmail.com>
2022-11-30 18:11:00 +0530

    basic objects in js practised, now going for loops, yipee

--- jsFundamentals/challenges.js
+++ jsFundamentals/challenges.js
@@ -9,3 +9,52 @@
 //     billValue + tipValue
 //   }`
 // );
+
+// tip calculator:
+// const calcTip = (billValue) =>
+//   billValue < 50 ? 0 : billValue <= 300 ? billValue * 0.15 : billValue * 0.2;
+// const bills = new Array(125, 555, 44, 50);
+// const tips = new Array();
+// const totalBillAmount = new Array();
+// bills.forEach((bill) => {
+//   const tip = calcTip(bill);
+//   tips.push(tip);
+//   totalBillAmount.push(bill + tip);
+// });
+// console.log("orignal bills are: ", bills);
+// console.log("tips calculated are: ", tips);
+// console.log("bill totals are: ", totalBillAmount);
+
+// const mark = {
+//   fullName: "Mark Miller",
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function () {
+//     this.bmi = this.mass / this.height ** 2; // **(power) has higher precedence than /(divide)
+//     return this.bmi;
+//   },
+// };
+
+// const john = {
+//   fullName: "John Smith",
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function () {
+//     this.bmi = this.mass / this.height ** 2; // **(power) has higher precedence than /(divide)
+//     return this.bmi;
+//   },
+// };
+
+// if (mark.calcBMI() > john.calcBMI()) {
+//   console.log(
+//     `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`
+//   );
+// } else if (john.bmi > mark.bmi) {
+//   console.log(
+//     `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`
+//   );
+// } else {
+//   console.log(
+//     `Both ${mark.fullName} and ${john.fullName} have same BMI's {${mark.bmi}}`
+//   );
+// }
--- jsFundamentals/script.js
+++ jsFundamentals/script.js
@@ -64,3 +64,68 @@ for (let i = 0; i < 100; i++) {
 //   str: "hi there",
 // };
 // console.log(numarr);
+
+// const friends = new Array("Divesh", "mulish", "Junaid", "prajjwal", "piyush");
+// const friends = ["Divesh", "mulish", "Junaid", "prajjwal", "piyush"];
+// console.log(...friends);
+// console.log(friends[5]);
+// console.log(friends.push("Lovish"));
+// console.log(friends);
+// console.log(friends.unshift("sexa"));
+// console.log(friends);
+// console.log(friends.shift());
+// console.log(friends);
+// console.log(friends.pop());
+// console.log(friends);
+// friends.push(23);
+// console.log(friends);
+// console.log(friends.includes(23)); //true
+// console.log(friends.includes("23")); //false
+// console.log(friends.indexOf("23")); //-1
+// console.log(friends.indexOf(23)); // index of 23
+
+// const name1 = "firstname";
+// const name2 = "lastname";
+// console.log(name1);
+// const objectVar = new Object({
+//   [name1]: "jatin goyal",
+// });
+// console.log(objectVar);
+
+const jatin = new Object({
+  firstName: "Jatin",
+  lastName: "Goyal",
+  age: 22,
+  job: "learner",
+  hasDrivingLicense: false,
+  friends: ["Divesh", "Mulish", "Piyush", "Juanid", "Prajjwal"],
+  // getSummary: (objectThis) =>
+  //   `${objectThis.firstName} is a ${objectThis.age} year old ${
+  //     objectThis.job
+  //   }, and he has ${
+  //     objectThis.hasDrivingLicense ? "a" : "no"
+  //   } driver's license`,
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.age} year old ${
+      this.job
+    }, and he has ${this.hasDrivingLicense ? "a" : "no"} driver's license`;
+  },
+});
+// const varr = 10;
+// console.log(jatin);
+const nameKey = "Name";
+// console.log(jatin["first" + nameKey]);
+// console.log(jatin["last" + nameKey]);
+const interestedIn = "job";
+// console.log(jatin.interestedIn); //undefined
+// console.log(jatin[interestedIn]); //actual value in obejct corresponding to key(variable's value)
+jatin.location = "Hanumangarh Town";
+jatin["instagram"] = "jatin7528";
+// console.log(jatin);
+// console.log(
+//   `${jatin.firstName} has ${jatin.friends.length} friends, and his best friend is ${jatin.friends[0]}`
+// );
+// console.log(jatin.getSummary); //ibject value: which is a function
+// console.log(jatin.getSummary(jatin)); // return value from function
+// console.log(jatin.getSummary()); // return value from function
